cb3df3e1465bed0047518f69a6d9b060ffce1fc5
din.nick@bk.ru <Rtyuehe89>
2019-05-27 23:50:21 +0500

    component

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
 angular.module('mcdatesApp', ['ngMaterial', 'ngMessages'])
+
+.component('mcDates', {
+    bindings: {
+        dateFrom: '=',
+        dateTo: '=',
+    },
+    template: `
+        <md-datepicker ng-model="$ctrl.dateFrom" md-open-on-focus="yes"></md-datepicker>
+        <md-datepicker ng-model="$ctrl.dateTo" md-open-on-focus="yes"></md-datepicker>
+    `,
+})
+
 .controller('McdatesController', function () {
-  this.myDate = new Date();
+  this.dateFrom = new Date();
+  this.dateTo = new Date();
 });
